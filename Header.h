7f31cf55bdbf82922e54f8aa33d03b4ab42369ee
kuffa2207 <61161188+kuffa2207@users.noreply.github.com>
2020-02-21 22:39:16 +0300

    ))

--- Header.h
+++ Header.h
@@ -1,27 +1,154 @@
+// Copyright 2020 pichugin.ilya
 #pragma once
+#include <iostream>
+#include <fstream>
+
 using namespace std;
-class Kvadrat {
+
+/* Объявление класса комплексных чисел */
+
+class Complex {
+
+  // публичные поля с полями и методами (у нас просто с методами), доступными пользователю  вашего класса
 public:
-  Kvadrat(double a, double b, double c, double d);
-  double P();
+  Complex();                                         // конструктор по умолчанию
+  Complex(const Complex &c);                         // конструктор копирования
+  Complex(double _re, double _im);                   // конструктор инициализации
+  Complex(double d);                                 // конструктор преобразования типа
+  ~Complex();                                        // деструктор
+
+  void output();                                     // функция вывода на экран комплексного числа
+  Complex Add(const Complex& c);                     // функция сложения
+
+  // перегрузка операторов
+  Complex& operator=(const Complex& c);              // перегруженный оператор присваивания
+
+  bool operator==(const Complex& c);                 // дз
+
+  Complex operator+(const Complex& c);               // перегруженные оператор сложения
+  Complex operator-(const Complex& c);               // дз
+  Complex operator*(const Complex& c);               // дз
+  Complex operator/(const Complex& c);               // дз
+
+  // Следующие функции рассмотрим на занятии вместе (можно попробовать самим вперёд)
+  friend Complex operator+(double d, const Complex& c);
+  friend Complex operator-(double d, const Complex& c);
+  friend Complex operator*(double d, const Complex& c);
+  friend Complex operator/(double d, const Complex& c);
+
+  friend ostream& operator<<(ostream& stream, const Complex& c);
+  friend istream& operator>>(istream& stream, Complex& c);
+
+  // скрытые от пользователя поля и методы (у нас просто поля)
 private:
-  double x1, y1, x2, y2;
-  double perimetr;
-};
+  double re;                                         // действительная часть числа
+  double im;                                         // мнимая часть числа
 
-Kvadrat::Kvadrat(double a, double b, double c, double d) {
-  x1 = a;
-  y1 = b;
-  x2 = c;
-  y2 = d;
+};                                                   // ВАЖНО: не забывайте ставить тут ;
 
-  if (x1 == x2 == y1 == y2) {
-    cout << "Координаты введены не правильно." << endl;
-  }
+/* Реализации конструкторов и деструктора*/
+
+/// конструктор по умолчанию
+Complex::Complex() {
+  re = 0.0;
+  this->im = 0.0;
 }
 
-double Kvadrat::P()
-{
-  perimetr = ((sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)))) + (sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1))))) * 2;
-  return perimetr;
+/// конструктор копирования
+Complex::Complex(const Complex &c) {
+  re = c.re;
+  im = c.im;
+}
+
+/// конструктор инициализации
+Complex::Complex(double _re, double _im) : re(_re), im(_im) {  }
+
+/// конструктор преобразования типа
+Complex::Complex(double _re) {
+  re = _re;
+  im = 0.0;
+}
+
+/// деструктор (удаление)
+Complex::~Complex() {
+  re = 0.0;
+  im = 0.0;
+}
+
+/* Реализации перегрузок операторов */
+
+/// оператор присваивания
+Complex& Complex::operator=(const Complex &c) {
+  (*this).re = c.re;
+  im = c.im;
+  return *this;
+}
+
+/// операторы сравнения
+bool Complex::operator==(const Complex &c) {
+  bool res = 0;
+  if (re == c.re && im == c.im) {
+    res = 1;
+  }
+  else {
+    res = 0;
+  }
+  return res;
+}
+
+/* Замечание: для комплексных чисел имеет смысл только эта операция сравнения
+              (поле комплексных чисел не может быть упорядочено), поэтому
+              перегрузка операторов > < >= <= не имеет смысла!
+*/
+
+/// функция сложения
+Complex Complex::Add(const Complex &c) {
+  Complex res;
+  res.re = re + c.re;
+  res.im = im + c.im;
+  return res;
+}
+
+/// оператор сложения
+Complex Complex::operator+(const Complex &c) {
+  Complex res;
+  res.re = re + c.re;
+  res.im = im + c.im;
+  return res;
+}
+
+/// оператор вычитания
+Complex Complex::operator-(const Complex &c) {
+  Complex res;
+  res.re = re - c.re;
+  res.im = im - c.im;
+  return res;
+}
+
+/// оператор умножения
+Complex Complex::operator*(const Complex &c) {
+  Complex res;
+  res.re = (re*c.re) - (im*c.im);
+  res.im = (im * c.re) + (c.im*re);
+  return res;
+}
+
+/// оператор деления
+Complex Complex::operator/(const Complex &c) {
+  Complex res;
+  res.re = (re*c.re + im*c.im) / (c.re*c.re + c.im*c.im);
+  res.im = (im * c.re - c.im*re) / (c.re*c.re + c.im*c.im);
+  return res;
+}
+
+/* Прочие функции */
+
+/// вывод комплексного числа на экран
+void Complex::output() {
+  cout << re;
+  if (im > 0) { cout << " + " << im << "i"; }
+  else {
+    if (im < 0) { cout << " - " << -im << "i"; }
+    cout << endl;
+  }
 }
